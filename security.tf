b9111b8d986654bde6c079fd3c708aa24ad3f8a4
ervaneet82 <36176808+ervaneet82@users.noreply.github.com>
2023-12-22 13:12:32 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region
+}
+
+variable "ingress_rules" {
+  type    = list(object({
+    type        = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      type        = "ingress"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      type        = "ingress"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    # Add more rules as needed...
+  ]
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Example security group with dynamic rules"
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      type        = ingress.value.type
+    }
+  }
+}
